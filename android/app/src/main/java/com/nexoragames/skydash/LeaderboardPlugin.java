9df9a4aff9ecd5950c169997e63ff9d4a19d9c64
dbstockman <stockmandaniel328@gmail.com>
2026-09-12 22:32:28 -0400

    Fix LeaderboardVariant import

--- android/app/src/main/java/com/nexoragames/skydash/LeaderboardPlugin.java
+++ android/app/src/main/java/com/nexoragames/skydash/LeaderboardPlugin.java
@@ -6,8 +6,8 @@ import com.getcapacitor.PluginCall;
 import com.getcapacitor.PluginMethod;
 import com.getcapacitor.annotation.CapacitorPlugin;
 import com.google.android.gms.common.api.ApiException;
-import com.google.android.gms.games.LeaderboardVariant;
 import com.google.android.gms.games.PlayGames;
+import com.google.android.gms.games.leaderboard.LeaderboardVariant;
 
 @CapacitorPlugin(name = "Leaderboard")
 public class LeaderboardPlugin extends Plugin {
